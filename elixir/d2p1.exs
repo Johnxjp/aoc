fdc5ba241d4b1ffc4ab5074228988325efa2c024
John Lingi <lingijohn23@gmail.com>
2020-02-16 12:16:56 +0000

    Day 2 part 1 v1

--- elixir/d2p1.exs
+++ elixir/d2p1.exs
@@ -1,36 +1,28 @@
-input = File.read!("./data/d2.txt") |> String.split(",") |> Enum.map(&String.to_integer/1)
-input = List.replace_at(input, 1, 12)
-input = List.replace_at(input, 2, 2)
-
 defmodule GravityAssist do
-  defp operate(list, []) do
-    list
-  end
+  defp parse(list, []), do: list
+  defp parse(list, [99 | _]), do: list
 
-  defp operate(list, [99 | _]) do
-    list
-  end
-
-  defp operate(list, [1 | tail]) do
+  defp parse(list, [code | tail]) do
     [a, b, store | remainder] = tail
-    val_a = Enum.at(list, a)
-    val_b = Enum.at(list, b)
-    list = List.replace_at(list, store, val_a + val_b)
-    operate(list, remainder)
-  end
+    [a, b] = [a, b] |> Enum.map(fn index -> Enum.at(list, index) end)
 
-  defp operate(list, [2 | tail]) do
-    [a, b, store | remainder] = tail
-    val_a = Enum.at(list, a)
-    val_b = Enum.at(list, b)
-    list = List.replace_at(list, store, val_a * val_b)
-    operate(list, remainder)
+    func =
+      case code do
+        1 -> &+/2
+        2 -> &*/2
+      end
+
+    list = List.replace_at(list, store, func.(a, b))
+    parse(list, remainder)
   end
 
   def parse(list) do
-    operate(list, list)
+    parse(list, list)
   end
 end
 
+input = File.read!("./data/d2.txt") |> String.split(",") |> Enum.map(&String.to_integer/1)
+input = List.replace_at(input, 1, 12)
+input = List.replace_at(input, 2, 2)
 output = GravityAssist.parse(input)
 IO.inspect(hd(output))
