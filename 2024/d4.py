e383df5bbe8b8d1814578b72415a0111da9f7e56
John Lingi <16599457+Johnxjp@users.noreply.github.com>
2025-01-16 14:57:04 +0000

    d4 p2

--- 2024/d4.py
+++ 2024/d4.py
@@ -102,6 +102,43 @@ def search(grid, keyword, start_x, start_y, height, width) -> int:
     return matches
 
 
+def search_2(grid, keyword, start_x, start_y, height, width) -> int:
+    """
+    Returns the number of times the keyword appears in the grid
+    when searching from the start position vertically, horizontally, diagonally
+    """
+
+    if grid[start_x][start_y] != "A":
+        return 0
+
+    # TL-BR
+    word = ""
+    for i in range(-1, 2):
+        curr_x, curr_y = start_x + i, start_y + i
+        if curr_x < 0 or curr_x >= height or curr_y < 0 or curr_y >= width:
+            return 0
+
+        word += grid[curr_x][curr_y]
+
+    if not (word == keyword or word[::-1] == keyword):
+        return 0
+
+    # BL-TR
+    word = ""
+    for i in range(-1, 2):
+        curr_x, curr_y = start_x - i, start_y + i
+        if curr_x < 0 or curr_x >= height or curr_y < 0 or curr_y >= width:
+            return 0
+
+        word += grid[curr_x][curr_y]
+
+    if not (word == keyword or word[::-1] == keyword):
+        return 0
+
+    print("Found X-MAS at", start_x, start_y)
+    return 1
+
+
 def d1():
     grid = load("d4.txt")
     height = len(grid)
@@ -115,5 +152,19 @@ def d1():
     print(total_matched)
 
 
+def d2():
+    grid = load("d4.txt")
+    height = len(grid)
+    width = len(grid[0])
+    keyword = "MAS"
+    total_matched = 0
+    for r in range(height):
+        for c in range(width):
+            total_matched += search_2(grid, keyword, r, c, height, width)
+
+    print(total_matched)
+
+
 if __name__ == "__main__":
     d1()
+    d2()
