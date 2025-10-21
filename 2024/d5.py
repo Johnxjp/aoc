672d1f9d6b9dcf7a86137203e2d0c2b8892f684b
jxp <16599457+Johnxjp@users.noreply.github.com>
2025-10-21 09:44:53 +0100

    Part 2. Define ordering class. Lean on python sort

--- 2024/d5.py
+++ 2024/d5.py
@@ -1,14 +1,46 @@
 from typing import Sequence
 import sys
 
+
 def is_correctly_ordered(update: Sequence[int], rules: dict[int, list[int]]) -> bool:
     for i, first in enumerate(update):
-        for second in update[i + 1:]:
+        for second in update[i + 1 :]:
             # If there is no rule for 'second' then accept
             if second in rules and first in rules[second]:
                 return False
-    
+
     return True
+
+
+class OrderedItems:
+
+    def __init__(self, item: int, rules: dict[int, list[int]]):
+        self.item = item
+        self.rules = rules
+
+    def __eq__(self, other):
+        """There is no equal"""
+        return False
+
+    def __lt__(self, other: "OrderedItems"):
+        """Less than, means should appear first which means other should be in list of values"""
+        # If item not in rules, then either rule doesn't exist or greater than or equal
+        # print(self.item, other.item, self.rules)
+        return self.item in self.rules and other.item in self.rules[self.item]
+
+    def __gt__(self, other: "OrderedItems"):
+        """item is in the list of values belonging to key other."""
+        # print(self.item, other.item, self.rules)
+        return other.item in self.rules and self.item in self.rules[other.item]
+
+    def __hash__(self):
+        return self.item
+
+
+def correct_update(update: Sequence[int], rules: dict[int, list[int]]) -> list[int]:
+    """This is a sorting algorithm but with rules. The check for greater or less than is based on rule ordering"""
+    update_custom = [OrderedItems(u, rules) for u in update]
+    return [u.item for u in sorted(update_custom)]
 
 
 if __name__ == "__main__":
@@ -27,11 +59,23 @@ if __name__ == "__main__":
 
     updates = [tuple(map(int, u.split(","))) for u in updates.splitlines()]
 
-    total = 0
+    # PART 1
+
+    total_p1 = 0
+    incorrect_updates = []
     for u in updates:
         if is_correctly_ordered(u, rule_map):
-           print("CORRECT", u)
-           total += u[len(u) // 2]
+            print("CORRECT", u)
+            total_p1 += u[len(u) // 2]
+        else:
+            incorrect_updates.append(u)
 
+    # PART 2
 
-    print(total)
+    total_p2 = 0
+    for u in incorrect_updates:
+        u_correct = correct_update(u, rule_map)
+        print(u, " CORRECTED TO ", u_correct)
+        total_p2 += u_correct[len(u) // 2]
+
+    print(total_p1, total_p2)
