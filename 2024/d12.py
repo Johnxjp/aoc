60b6e583809b1d2a56ea12b9f9a41d35ca612aac
jxp <16599457+Johnxjp@users.noreply.github.com>
2025-10-30 15:36:56 +0000

    Part 2. Disgusting algorithm

--- 2024/d12.py
+++ 2024/d12.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 from typing import Tuple
 
 
@@ -5,18 +6,54 @@ def load_grid(filename: str):
     grid = []
     with open(filename) as f:
         for l in f:
-            grid.append(
-                [c for c in l.strip()]
-            )
+            grid.append([c for c in l.strip()])
     return grid
 
 
-def calculate_area():
-    pass
+def number_sides(region_coords: set[Tuple[int, int]]) -> int:
+    perimeter = set()
+    sides = []
+    sorted_coords = sorted(list(region_coords), key=itemgetter(0, 1))
+    # print(sorted_coords)
+    for r, c in sorted_coords:
+        if (r + 1, c) not in region_coords:
+            if (
+                (r + 1, c - 1, "N") not in perimeter
+                and (r + 1, c + 1, "N") not in perimeter
+                and (r + 1, c, "N") not in perimeter
+            ):
+                sides.append((r + 1, c))
+            perimeter.add((r + 1, c, "N"))
 
+        if (r - 1, c) not in region_coords:
+            if (
+                (r - 1, c - 1, "S") not in perimeter
+                and (r - 1, c + 1, "S") not in perimeter
+                and (r - 1, c, "S") not in perimeter
+            ):
+                sides.append((r - 1, c))
+            perimeter.add((r - 1, c, "S"))
 
-def calculate_region_fence_cost():
-    pass
+        if (r, c + 1) not in region_coords:
+            if (
+                (r - 1, c + 1, "E") not in perimeter
+                and (r + 1, c + 1, "E") not in perimeter
+                and (r, c + 1, "E") not in perimeter
+            ):
+                sides.append((r, c + 1))
+            perimeter.add((r, c + 1, "E"))
+
+        if (r, c - 1) not in region_coords:
+            if (
+                (r - 1, c - 1, "W") not in perimeter
+                and (r + 1, c - 1, "W") not in perimeter
+                and (r, c - 1, "W") not in perimeter
+            ):
+                sides.append((r, c - 1))
+            perimeter.add((r, c - 1, "W"))
+
+    # print(len(perimeter), sides, len(sides))
+    return len(sides)
 
 
 def get_neighbours(grid, r, c, n_rows, n_cols):
@@ -53,31 +90,25 @@ def find_region(grid: list[list[str]], r: int, c: int) -> set[Tuple[int, int]]:
 
 def find_perimeter(region_coords: set[Tuple[int, int]]) -> int:
     perimeter = 0
-    for (r, c) in region_coords:
+    for r, c in region_coords:
         if (r + 1, c) not in region_coords:
             perimeter += 1
-        
+
         if (r - 1, c) not in region_coords:
             perimeter += 1
 
-        if (r, c + 1) not in region_coords: 
+        if (r, c + 1) not in region_coords:
             perimeter += 1
 
-        if (r, c - 1) not in region_coords: 
+        if (r, c - 1) not in region_coords:
             perimeter += 1
 
     return perimeter
 
 
 def p1(input: str):
-
-    # count_areas = {}
-    # for i in range(len(input)):
-    #     for j in range(len(input[0])):
-    #         c = input[i][j]
-    #         count_areas[c] = count_areas.get(c, 0) + 1
-
     total_cost = 0
+    total_discounted = 0
     visited_regions = set()
     for i in range(len(input)):
         for j in range(len(input[0])):
@@ -91,11 +122,15 @@ def p1(input: str):
             area = len(region_coords)
             # count_areas[c] -= area
             perimeter = find_perimeter(region_coords)
+            sides = number_sides(region_coords)
             cost = area * perimeter
+            discounted = area * sides
             total_cost += cost
-            print(c, (i, j), area, perimeter, cost)
+            total_discounted += discounted
+            # print(c, (i, j), area, perimeter, sides, cost, discounted)
 
-    return total_cost
+    return total_cost, total_discounted
+
 
 if __name__ == "__main__":
 
@@ -103,8 +138,14 @@ if __name__ == "__main__":
     sample = [[c for c in s] for s in sample.split("\n")]
     sample_2 = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE"
     sample_2 = [[c for c in s] for s in sample_2.split("\n")]
+    sample_3 = "EEEEE\nEXXXX\nEEEEE\nEXXXX\nEEEEE"
+    sample_3 = [[c for c in s] for s in sample_3.split("\n")]
+    sample_4 = "AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA"
+    sample_4 = [[c for c in s] for s in sample_4.split("\n")]
     grid = load_grid("./2024/d12.txt")
 
     print(p1(sample))
     print(p1(sample_2))
+    print(p1(sample_3))
+    print(p1(sample_4))
     print(p1(grid))
